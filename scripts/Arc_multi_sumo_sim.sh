85f08838dd03e77796914930dab086e0fc1d9522
Dong Li <dongli@vt.edu>
2023-05-01 22:53:20 -0400

    update Arc scripts

--- scripts/Arc_multi_sumo_sim.sh
+++ scripts/Arc_multi_sumo_sim.sh
@@ -2,7 +2,7 @@
 #SBATCH -J SUMO_SIM
 #SBATCH --account=ev_charging
 #SBATCH --partition=normal_q
-#SBATCH --nodes=1 --ntasks-per-node=40 --cpus-per-task=1
+#SBATCH --nodes=1 --ntasks-per-node=24 --cpus-per-task=1
 #SBATCH --time=0-08:00:00 # 6 hours
 
 #SBATCH --mail-user=dongli@vt.edu
@@ -13,20 +13,25 @@
 echo "Current job id is: $SLURM_JOB_ID"
 
 ## change directory and load modules ##
-ROOT_PATH=/home/dong/workspace/intersection_CDG
+ROOT_PATH=/home/dongli/workspace/intersection_CDG
+# ROOT_PATH=/home/dong/workspace/intersection_CDG
 cd $ROOT_PATH/release
 module reset
-module load module load SUMO/1.14.1-foss-2021b
+module load yaml-cpp
+module load SUMO/1.14.1-foss-2021b
 
 ## No more needed after success compilation ##
+module load CMake/3.21.1-GCCcore-11.2.0
 cmake -DCMAKE_BUILD_TYPE=RELEASE ..
 make sumo_sim_batch
 
-N=(5 10 20 50 100)
+N=(5 10 30 100)
 geometryIDList=(1 2)
-arrivalIntervalList=(1 2 3 4)
+arrivalIntervalList=(1 2 3)
 
-cd $ROOT_PATH/log/sumosim
+cd $ROOT_PATH/log
+mkdir sumosim
+cd sumosim
 
 for numVehicle in ${N[@]}; do
     if [[ ! -d N${numVehicle} ]]; then
@@ -39,8 +44,8 @@ cd $ROOT_PATH/release
 for numVehicle in ${N[@]}; do
     for geometryID in ${geometryIDList[@]}; do
         for arrival_interval_avg in ${arrivalIntervalList[@]}; do
-            echo "./sumo_sim/sumo_sim_batch --num_nodes $numVehicle --geometryID ${geometryID} --arrival_interval_avg ${arrival_interval_avg} &"
-            ./sumo_sim/sumo_sim_batch --num_nodes $numVehicle --geometryID ${geometryID} --arrival_interval_avg ${arrival_interval_avg} &
+            echo "./sumo_sim/sumo_sim_batch --num_nodes $numVehicle --geometryID ${geometryID} --arrival_interval_avg ${arrival_interval_avg} &"  # --test_count 1
+            ./sumo_sim/sumo_sim_batch --num_nodes $numVehicle --geometryID ${geometryID} --arrival_interval_avg ${arrival_interval_avg} &  # --test_count 1
             sleep 2
         done
     done
