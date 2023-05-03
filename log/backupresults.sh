863bd541d2fee2b9aa99220603e15da89af9f7e3
Dong Li <dongli@vt.edu>
2023-05-02 21:45:28 -0400

    update backup scripts

--- log/backupresults.sh
+++ log/backupresults.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tar_ball_name=Arc_sumosim-$(date +"%Y%m%d%H%M%S").tar.gz
+tar_ball_name=sumosim_backup-$(date +"%Y%m%d%H%M%S").tar.gz
 target_dir=sumosim
 
 echo "back up in file: backup/$tar_ball_name"
